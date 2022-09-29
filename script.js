6191546555673d76a52ef9d55e2dd71e1c3aeb28
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-09-30 00:54:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+window.onscroll = () =>{
+   const value = window.scrollY / 8;
+   const het = document.body.offsetHeight;
+   const nav = document.getElementsByClassName("side-nav")[0];
+ document.getElementById("moon").style.bottom = `${value}%`;
+ document.getElementById("moon").style.marginLeft = `${value}%`;   
+};
+
+
+setTimeout(function loda(){
+const x = document.getElementById("loader");
+x.style.bottom ="100%";
+document.body.classList.remove("display");
+},5000);
+
+
+
+
+setInterval(function ashutosh(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (30 - 1 + 1)) + 1;				
+document.getElementById("sec-drop1").style.left = l + "%";
+document.getElementById("sec-drop1").style.top = t + "%";
+},2000);
+setInterval(function ashutos(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (30 - 1 + 1)) + 1;				
+document.getElementById("sec-drop2").style.left = l + "%";
+document.getElementById("sec-drop2").style.top = t + "%";
+},2000);
+setInterval(function ashuto(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (30 - 1 + 1)) + 1;				
+document.getElementById("sec-drop3").style.left = l + "%";
+document.getElementById("sec-drop3").style.top = t + "%";
+},2000);
+setInterval(function ashut(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (30 - 1 + 1)) + 1;				
+document.getElementById("sec-drop4").style.left = l + "%";
+document.getElementById("sec-drop4").style.top = t + "%";
+},2000);
+setInterval(function ashu(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (30 - 1 + 1)) + 1;			
+document.getElementById("sec-drop5").style.left = l + "%";
+document.getElementById("sec-drop5").style.top = t + "%";
+},2000);
+setInterval(function ash(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (30 - 1 + 1)) + 1;				
+document.getElementById("sec-drop6").style.left = l + "%";
+document.getElementById("sec-drop6").style.top = t + "%";
+},2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+setInterval(function ashutosha(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (100 - 70 + 1)) + 70; 				
+document.getElementById("sec-drop7").style.left = l + "%";
+document.getElementById("sec-drop7").style.top = t + "%";
+},2000);
+setInterval(function ashutosa(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (100 - 70 + 1)) + 70; 				
+document.getElementById("sec-drop8").style.left = l + "%";
+document.getElementById("sec-drop8").style.top = t + "%";
+},2000);
+setInterval(function ashutoa(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (100 - 70 + 1)) + 70; 				
+document.getElementById("sec-drop9").style.left = l + "%";
+document.getElementById("sec-drop9").style.top = t + "%";
+},2000);
+setInterval(function ashuta(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (100 - 70 + 1)) + 70; 				
+document.getElementById("sec-drop10").style.left = l + "%";
+document.getElementById("sec-drop10").style.top = t + "%";
+},2000);
+setInterval(function ashua(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (100 - 70 + 1)) + 70; 				
+document.getElementById("sec-drop11").style.left = l + "%";
+document.getElementById("sec-drop11").style.top = t + "%";
+},2000);
+setInterval(function asha(){
+var l = Math.floor(Math.random() * 100);
+var t = Math.floor(Math.random() * (100 - 70 + 1)) + 70; 				
+document.getElementById("sec-drop12").style.left = l + "%";
+document.getElementById("sec-drop12").style.top = t + "%";
+},2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
